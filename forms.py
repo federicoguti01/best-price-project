40a48b434ca5f99bda1079e6e5a1c32915978103
Federico Gutierrez <fg42768n@pace.edu>
2021-07-22 19:32:33 +0000

    fixing style
    Co-authored-by: Ari Hamilton <alh33286@uga.edu>
    Co-authored-by: Tinsae Dejene <tens2009et@gmail.com>

--- forms.py
+++ forms.py
@@ -5,34 +5,42 @@ from wtforms import Form, SelectField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 from flask import Flask, render_template, url_for, flash, redirect
 
-#create some fields
+# create some fields
+
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username',
                            validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField('Email',
                         validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired(), Length(min=6, max=50)])
-    confirm_password = PasswordField('Confirm Password',
-                                     validators=[DataRequired(), EqualTo('password')])
+    password = PasswordField(
+        'Password', validators=[
+            DataRequired(), Length(
+                min=6, max=50)])
+    confirm_password = PasswordField(
+        'Confirm Password', validators=[
+            DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
-    
+
+
 class LoginForm(FlaskForm):
     username = StringField('Username',
                            validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember me')
     submit = SubmitField('Sign in')
-    
-class ProductSearchForm(FlaskForm):
-#     choices = [('Artist', 'Artist'),
-#                ('Album', 'Album'),
-#                ('Publisher', 'Publisher')]
 
-#     select = SelectField('Search for music:', choices=choices)
+
+class ProductSearchForm(FlaskForm):
+    #     choices = [('Artist', 'Artist'),
+    #                ('Album', 'Album'),
+    #                ('Publisher', 'Publisher')]
+
+    #     select = SelectField('Search for music:', choices=choices)
     search = StringField('')
     submit = SubmitField('Search')
-    
+
+
 class AltSelectForm(FlaskForm):
     choices = [(0, '0'),
                (1, '1'),
